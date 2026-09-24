bdde6c6d2c7043e4f8305d4cd876bc1c54a6b4da
lakindugimsara50-del <lakindugimsara50@gmail.com>
2026-09-24 23:56:44 +0530

    Sync movies_data.js for The 100

--- website/data/movies_data.js
+++ website/data/movies_data.js
@@ -1321,8 +1321,213 @@ window.FILMSUB_DATA = {
       "added_date": "2026-09-24",
       "added_at": "2026-09-24T17:37:56Z",
       "added_by": "bot_auto_leech"
+    },
+    {
+      "id": "the-100-2014-s01e02",
+      "slug": "the-100-2014-s01e02",
+      "title": "The 100",
+      "title_si": "",
+      "year": 2014,
+      "imdb": "N/A",
+      "rating": "N/A",
+      "imdb_id": "tt2661044",
+      "tmdb_id": "48866",
+      "type": "series",
+      "season": 1,
+      "episode": 2,
+      "episode_title": "Earth Skills",
+      "number_of_seasons": 7,
+      "number_of_episodes": 100,
+      "seasons": [
+        {
+          "season_number": 1,
+          "name": "Season 1",
+          "episode_count": 13,
+          "poster_url": "https://image.tmdb.org/t/p/w500/jAsSkxSGEjeEEHL3KiWXr3NwyPG.jpg",
+          "air_date": "2014-03-19"
+        },
+        {
+          "season_number": 2,
+          "name": "Season 2",
+          "episode_count": 16,
+          "poster_url": "https://image.tmdb.org/t/p/w500/4ngujsHppz4Imxh7qf9NQIIMlLY.jpg",
+          "air_date": "2014-10-22"
+        },
+        {
+          "season_number": 3,
+          "name": "Season 3",
+          "episode_count": 16,
+          "poster_url": "https://image.tmdb.org/t/p/w500/ayebqASxTXVSo9Qoug6M5YowZS0.jpg",
+          "air_date": "2016-01-21"
+        },
+        {
+          "season_number": 4,
+          "name": "Season 4",
+          "episode_count": 13,
+          "poster_url": "https://image.tmdb.org/t/p/w500/qCdJvikZUQkr4sOVYNkKHntRfha.jpg",
+          "air_date": "2017-02-01"
+        },
+        {
+          "season_number": 5,
+          "name": "Season 5",
+          "episode_count": 13,
+          "poster_url": "https://image.tmdb.org/t/p/w500/oYhQz0jHC3pgJKNMyKXiLKauQUC.jpg",
+          "air_date": "2018-04-24"
+        },
+        {
+          "season_number": 6,
+          "name": "Season 6",
+          "episode_count": 13,
+          "poster_url": "https://image.tmdb.org/t/p/w500/wBzNjurA8ijJPF21Ggs9nbviIzi.jpg",
+          "air_date": "2019-04-30"
+        },
+        {
+          "season_number": 7,
+          "name": "Season 7",
+          "episode_count": 16,
+          "poster_url": "https://image.tmdb.org/t/p/w500/xLOEWox9zuvzDbDdODP92RnQtjO.jpg",
+          "air_date": "2020-05-20"
+        }
+      ],
+      "poster": "https://image.tmdb.org/t/p/w500/wHIMMLFsk32wIzDmawWkYVbxFCS.jpg",
+      "poster_url": "https://image.tmdb.org/t/p/w500/wHIMMLFsk32wIzDmawWkYVbxFCS.jpg",
+      "backdrop": "https://image.tmdb.org/t/p/original/8ZerYKvIaNUJZvAHXYTQu4qTwFw.jpg",
+      "backdrop_url": "https://image.tmdb.org/t/p/original/8ZerYKvIaNUJZvAHXYTQu4qTwFw.jpg",
+      "genres": [
+        "Sci-Fi & Fantasy",
+        "Action & Adventure",
+        "Drama",
+        "Crime"
+      ],
+      "language": "English",
+      "subtitle_language": "Sinhala",
+      "has_sinhala_sub": true,
+      "sub_merged": true,
+      "quality": "1080p",
+      "duration": "45 min",
+      "description": "100 years in the future, when the Earth has been abandoned due to radioactivity, the last surviving humans live on an ark orbiting the planet — but the ark won't last forever. So the repressive regime picks 100 expendable juvenile delinquents to send down to Earth to see if the planet is still habitable.",
+      "director": "Jason Rothenberg",
+      "cast": [
+        {
+          "name": "Eliza Taylor",
+          "character": "Clarke Griffin"
+        },
+        {
+          "name": "Bob Morley",
+          "character": "Bellamy Blake"
+        },
+        {
+          "name": "Lindsey Morgan",
+          "character": "Raven Reyes"
+        },
+        {
+          "name": "Marie Avgeropoulos",
+          "character": "Octavia Blake"
+        },
+        {
+          "name": "Richard Harmon",
+          "character": "John Murphy"
+        }
+      ],
+      "featured": false,
+      "trending": true,
+      "file_id": "BAACAgUAAyEGAAMBAdXaEQADL2q1a2Fh0Tf4iVCtg2lOrIGL4unXAAL5JAACTLaxVS8HAcrqzg7fHgQ",
+      "message_id": 47,
+      "file_name": "the-100-2014-s01e02.mp4",
+      "file_size": 1641455851,
+      "stream_url": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview",
+      "streams": [
+        {
+          "server": "Server 1",
+          "label": "⚡ Server 1 (Google Drive Ultra HD + Auto Sub)",
+          "type": "video/mp4",
+          "stream_url": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview",
+          "quality": "1080p"
+        },
+        {
+          "server": "Server 2",
+          "label": "🎬 Server 2 (Direct Player • Auto Sinhala Sub)",
+          "type": "video/mp4",
+          "stream_url": "https://drive.google.com/uc?export=download&id=1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S",
+          "quality": "1080p"
+        }
+      ],
+      "qualities": {
+        "auto": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview",
+        "1080p": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview?vq=hd1080",
+        "720p": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview?vq=hd720",
+        "480p": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview?vq=large",
+        "360p": "https://drive.google.com/file/d/1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S/preview?vq=medium"
+      },
+      "downloads": [
+        {
+          "quality": "1080p",
+          "label": "1080p Full HD (Sinhala Sub Merged)",
+          "size": "1.5 GB",
+          "url": "https://drive.google.com/uc?export=download&id=1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S",
+          "format": "MP4",
+          "host": "Google Drive",
+          "sub_merged": true,
+          "subtitle_merged": true
+        },
+        {
+          "quality": "720p",
+          "label": "720p HD (Sinhala Sub Merged)",
+          "size": "861.0 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S&vq=hd720",
+          "format": "MP4",
+          "host": "Google Drive",
+          "sub_merged": true,
+          "subtitle_merged": true
+        },
+        {
+          "quality": "480p",
+          "label": "480p SD (Sinhala Sub Merged)",
+          "size": "438.3 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S&vq=large",
+          "format": "MP4",
+          "host": "Google Drive",
+          "sub_merged": true,
+          "subtitle_merged": true
+        },
+        {
+          "quality": "360p",
+          "label": "360p Data Saver (Sinhala Sub Merged)",
+          "size": "281.8 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1cZlVhwkP-Z5LASckoGQ5qWHOHfiQUT5S&vq=medium",
+          "format": "MP4",
+          "host": "Google Drive",
+          "sub_merged": true,
+          "subtitle_merged": true
+        },
+        {
+          "quality": "1080p (Telegram Download)",
+          "label": "1080p Full HD (Telegram • Sinhala Sub Merged)",
+          "size": "1.5 GB",
+          "url": "https://stream.yourdomain.workers.dev/stream/channel/-1004325759505/47",
+          "format": "MP4",
+          "host": "Telegram",
+          "download_only": true,
+          "sub_merged": true,
+          "subtitle_merged": true
+        }
+      ],
+      "subtitles": [
+        {
+          "language": "Sinhala",
+          "srclang": "si",
+          "label": "සිංහල උපසිරැසි (Sinhala)",
+          "url": "data:text/vtt;charset=utf-8,WEBVTT%0A%0A00%3A00%3A01.000%20--%3E%2000%3A00%3A06.000%0AFilmSub.lk%20%E2%80%94%20%E0%B7%83%E0%B7%92%E0%B6%82%E0%B7%84%E0%B6%BD%20%E0%B6%8B%E0%B6%B4%E0%B7%83%E0%B7%92%E0%B6%BB%E0%B7%90%E0%B7%83%E0%B7%92%20%E0%B7%83%E0%B6%B8%E0%B6%9F%20%28Auto%20Sinhala%20Subtitles%29%0A%0A00%3A01%3A57.575%20--%3E%2000%3A02%3A01.996%0ABaunei%20is%20a%20well-located%20village%0A%0A00%3A02%3A02.497%20--%3E%2000%3A02%3A08.461%0Ain%20a%20place%20of%20hard%20stones%2C%0A%0A00%3A02%3A08.545%20--%3E%2000%3A02%3A11.464%0Aabove%20the%20plain%2C%0A%0A00%3A02%3A11.548%20--%3E%2000%3A02%3A15.051%0Aunder%20the%20mountain%2C%20and%20in%20the%20middle%20of%20the%20coast.%0A%0A00%3A02%3A16.427%20--%3E%2000%3A02%3A20.640%0AEveryone%20who%20goes%20by%20makes%20a%20stop%20for%20its%20beauty.%0A%0A00%3A02%3A20.723%20--%3E%2000%3A02%3A22.684%0AIt%20looks%20so%20good%21%0A%0A00%3A02%3A22.767%20--%3E%2000%3A02%3A25.061%0AAll%20those%20who%20pass%20by%20crave%20it%0A%0A00%3A02%3A25.145%20--%3E%2000%3A02%3A30.775%0Afor%20its%20beauty%20and%20its%20view.%0A%0A00%3A03%3A16.404%20--%3E%2000%3A03%3A18.448%0A-%20Good%20morning.%20-Good%20morning.%0A%0A00%3A03%3A18.531%20--%3E%2000%3A03%3A21.409%0ANow%20you%20show%20up%3F%20We%20already%20finished%20cooking%21%0A%0A00%3A03%3A21.492%20--%3E%2000%3A03%3A23.661%0A-%20A%20little%20fiesta%21%20-%20Good%20morning%2C%20ma%27am.%0A%0A00%3A03%3A23.745%20--%3E%2000%3A03%3A25.872%0AGood%20morning.%0A%0A00%3A03%3A25.955%20--%3E%2000%3A03%3A27.415%0AWhat%20are%20you%20making%3F%0A%0A00%3A03%3A27.498%20--%3E%2000%3A03%3A28.833%0AGathulis.%0A%0A00%3A03%3A28.917%20--%3E%2000%3A03%3A30.210%0AIs%20it%20sweet%3F%0A%0A00%3A03%3A30.293%20--%3E%2000%3A03%3A33.504%0ANo%2C%20it%27s%20not%20sweet.%0A%0A00%3A03%3A33.588%20--%3E%2000%3A03%3A35.965%0APotato%20and%20cheese.%0A%0A00%3A03%3A42.722%20--%3E%2000%3A03%3A45.642%0AIf%20you%20had%20to%20judge%20your%20own%20health%2C%0A%0A00%3A03%3A45.725%20--%3E%2000%3A03%3A50.230%0Awould%20you%20say%20it%27s%20bad%2C%20good%2C%20very%20good%20or%20excellent%3F%0A%0A00%3A03%3A50.313%20--%3E%2000%3A03%3A52.607%0AFor%20the%20age%20I%20have%2C%20I%27d%20say%20it%27s%20good.%0A%0A00%3A03%3A52.690%20--%3E%2000%3A03%3A55.610%0A-%20Good%3F%20Not%20excellent%3F%20-Excellent.%20Maybe%20excellent.%0A%0A00%3A03%3A57.695%20--%3E%2000%3A03%3A59.697%0AIt%20can%27t%20be%20absolutely%20excellent.%0A%0A00%3A04%3A05.078%20--%3E%2000%3A04%3A09.457%0AFor%20me%2C%20as%20I%20said%20before%2C%20faith%20and%20hope%20are%20the%20two.%0A%0A00%3A04%3A09.540%20--%3E%2000%3A04%3A13.044%0ASo%20it%27s%20that.%20It%27s%20not%20down%20to%20my%20merit.%0A%0A00%3A04%3A18.174%20--%3E%2000%3A04%3A20.760%0AIf%20I%20walk%20slowly%2C%20ten%20minutes.%0A%0A00%3A04%3A31.104%20--%3E%2000%3A04%3A33.940%0AWhen%20I%20was%20a%20kid%2C%20there%20were%20only%20wheat%20fields%20here.%0A%0A00%3A04%3A34.941%20--%3E%2000%3A04%3A37.110%0ADo%20you%20remember%20the%20post%20office%20used%20to%20be%20here%3F%0A%0A00%3A04%3A37.193%20--%3E%2000%3A04%3A38.903%0A-%20Yes.%20-Mrs.%20Lavinia%20worked%20there.%0A%0A00%3A04%3A38.987%20--%3E%2000%3A04%3A41.197%0AYes%2C%20I%20remember%20that.%0A%0A00%3A04%3A42.699%20--%3E%2000%3A04%3A44.659%0A-%20Good%20evening%21%20-Hello%21%0A%0A00%3A04%3A44.742%20--%3E%2000%3A04%3A46.744%0AWhere%20are%20you%20going%3F%20Out%20for%20a%20walk%3F%0A%0A00%3A08%3A27.798%20--%3E%2000%3A08%3A29.133%0ADid%20it%20heat%20up%3F%0A%0A00%3A08%3A30.426%20--%3E%2000%3A08%3A32.720%0AIt%20won%27t%20be%20easy%20to%20warm%20it%20up%20quickly.%0A%0A00%3A08%3A32.803%20--%3E%2000%3A08%3A37.517%0AYou%20have%20to%20stir%20it%2C%20then%20leave%20it%20alone%20for%20a%20little%20while.%0A%0A00%3A08%3A37.600%20--%3E%2000%3A08%3A39.018%0AYou%27re%20really%20not%20good%20at%20this%21%0A%0A00%3A08%3A41.020%20--%3E%2000%3A08%3A42.688%0AWhere%20is%20the%20salt%3F%0A%0A00%3A08%3A44.273%20--%3E%2000%3A08%3A45.149%0AAnd%20the%20onion%3F%0A%0A00%3A10%3A26.500%20--%3E%2000%3A10%3A27.918%0ACheck%20this%20out%21%0A%0A00%3A12%3A07.435%20--%3E%2000%3A12%3A13.190%0AI%20had%20two%20aunts.%20One%20died%20at%20103%2C%20another%20102.%0A%0A00%3A12%3A16.318%20--%3E%2000%3A12%3A17.445%0AHe%27s%20an%20old%20man%21%0A%0A00%3A12%3A18.738%20--%3E%2000%3A12%3A21.157%0AI%27m%20not%20yet%20as%20old%20as%20he%20says.%0A%0A00%3A12%3A58.110%20--%3E%2000%3A13%3A01.614%0AFirst%2C%20we%20milked%20the%20animals%0A%0A00%3A13%3A02.573%20--%3E%2000%3A13%3A04.366%0Aearly%20when%20it%20started%20to%20get%20light.%0A%0A00%3A13%3A16.212%20--%3E%2000%3A13%3A17.838%0AThe%20wall%20wasn%27t%20there%20yet%2C%0A%0A00%3A13%3A18.923%20--%3E%2000%3A13%3A21.425%0Aso%20we%20had%20to%20follow%20the%20animals%20everywhere.%0A%0A00%3A16%3A37.413%20--%3E%2000%3A16%3A40.332%0AI%20never%20liked%20going%20to%20school%20for%20writing.%0A%0A00%3A16%3A41.667%20--%3E%2000%3A16%3A44.294%0ANow%20she%20won%27t%20give%20you%20back%20your%20pen.%0A%0A00%3A16%3A44.378%20--%3E%2000%3A16%3A47.006%0AShe%27s%20on%20the%20last%20one.%0A%0A00%3A16%3A51.051%20--%3E%2000%3A16%3A52.386%0AWhen%20I%20was%2080%2C%0A%0A00%3A16%3A52.469%20--%3E%2000%3A16%3A55.848%0Aa%20woman%20asked%20me%20about%20my%20retirement%20pension.%0A%0A00%3A16%3A55.931%20--%3E%2000%3A16%3A59.977%0AEveryone%20in%20my%20family%20was%20dying%20in%20their%2080s%2C%0A%0A00%3A17%3A00.060%20--%3E%2000%3A17%3A02.479%0Aso%20I%20said%2C%20%22What%27s%20the%20point%20of%20asking%20for%20a%20pension%3F%22%0A%0A00%3A17%3A02.563%20--%3E%2000%3A17%3A07.276%0AAnd%20now%20I%27ve%20missed%20those%2021%20years%20of%20pension%20payments.%0A%0A00%3A17%3A07.359%20--%3E%2000%3A17%3A09.737%0ABut%20you%27ve%20gained%20in%20age.%0A%0A00%3A17%3A11.238%20--%3E%2000%3A17%3A13.907%0A-%20Has%20she%20always%20been%20single%3F%20-%20Yes.%0A%0A00%3A17%3A13.991%20--%3E%2000%3A17%3A16.285%0AHave%20you%20always%20been%20single%2C%20Aunt%20Gio%3F%0A%0A00%3A17%3A16.368%20--%3E%2000%3A17%3A19.455%0A-Yes.%20-True%2C%20she%20has%20always%20been%20single.%0A%0A00%3A17%3A21.665%20--%3E%2000%3A17%3A25.502%0AWe%20take%20turns%20taking%20care%20of%20her%20every%20day.%0A%0A00%3A17%3A25.586%20--%3E%2000%3A17%3A28.672%0AWe%20take%20her%20for%20a%20walk.%20We%20cook%20for%20her.%0A%0A00%3A17%3A29.339%20--%3E%2000%3A17%3A32.926%0AEven%20when%20she%20was%20younger%2C%20she%20was%20invited%20to%20lunch%20by%20all%20her%20nieces.%0A%0A00%3A17%3A33.010%20--%3E%2000%3A17%3A36.221%0AWe%20never%20left%20her%20alone%20on%20Sundays%2C%0A%0A00%3A17%3A36.305%20--%3E%2000%3A17%3A40.059%0Adespite%20us%20being%20busy%20with%20our%20kids.%0A%0A00%3A17%3A40.142%20--%3E%2000%3A17%3A43.979%0AWe%20always%20considered%20her%20like%20a%20mother%20to%20us.%0A%0A00%3A17%3A45.105%20--%3E%2000%3A17%3A49.026%0AShe%20doesn%27t%20have%20children%20of%20her%20own%2C%20but%20she%20took%20care%20of%20us.%0A%0A00%3A17%3A49.818%20--%3E%2000%3A17%3A52.237%0AOnce%2C%20she%20was%20very%20ill.%0A%0A00%3A17%3A53.697%20--%3E%2000%3A17%3A55.199%0AWe%20hospitalized%20her%2C%0A%0A00%3A17%3A55.699%20--%3E%2000%3A18%3A00.579%0Aand%20at%20the%20hospital%2C%20she%20was%20always%20turned%20towards%20the%20wall%0A%0A00%3A18%3A00.662%20--%3E%2000%3A18%3A02.915%0Aand%20wouldn%27t%20speak%20to%20anyone.%0A%0A00%3A18%3A02.998%20--%3E%2000%3A18%3A06.668%0AShe%20wouldn%27t%20say%20anything%2C%20and%20we%20knew%20we%20were%20losing%20her.%0A%0A00%3A18%3A06.752%20--%3E%2000%3A18%3A10.589%0ASo%20we%20started%20taking%20turns%20in%20groups%2C%0A%0A00%3A18%3A10.672%20--%3E%2000%3A18%3A12.216%0Aand%20we%20fed%20her.%0A%0A00%3A18%3A12.299%20--%3E%2000%3A18%3A13.842%0AWe%20held%20her%20head%20up%0A%0A00%3A18%3A13.926%20--%3E%2000%3A18%3A15.844%0Abecause%20she%20couldn%27t%20stand%20up.%0A%0A00%3A20%3A23.805%20--%3E%2000%3A20%3A28.227%0AYou%20want%20to%20know%20something%3F%20When%20we%20come%20here%2C%20we%20relax.%0A%0A00%3A20%3A29.228%20--%3E%2000%3A20%3A34.024%0AWe%20get%20to%20relax%20because%20she%20transmits%20serenity.%0A%0A00%3A37%3A13.898%20--%3E%2000%3A37%3A16.442%0APlease%20protect%20us.%20I%20ask%20for%20protection.%0A%0A00%3A37%3A16.525%20--%3E%2000%3A37%3A18.361%0APlease%20watch%20over%20us.%0A%0A00%3A37%3A18.444%20--%3E%2000%3A37%3A20.154%0AThat%27s%20what%20I%20pray%20for.%0A%0A00%3A37%3A20.238%20--%3E%2000%3A37%3A21.197%0AKeep%20us%20healthy.%0A%0A00%3A37%3A21.739%20--%3E%2000%3A37%3A22.949%0AFor%20my%20grandchildren.%0A%0A00%3A37%3A23.449%20--%3E%2000%3A37%3A24.659%0ATo%20be%20safe.",
+          "default": true
+        }
+      ],
+      "source_method": "torrent",
+      "site_url": "https://filmsub.pages.dev/movie.html?id=the-100-2014-s01e02",
+      "added_date": "2026-09-24",
+      "added_at": "2026-09-24T18:14:47Z",
+      "added_by": "bot_auto_leech"
     }
   ],
-  "last_updated": "2026-09-24T17:46:15Z"
+  "last_updated": "2026-09-24T18:26:43Z"
 };
 window.MOVIES_DATA = window.FILMSUB_DATA;

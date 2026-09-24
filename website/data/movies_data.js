d356528c931540eefa4b2eb7f8f11d5a5ad2a0a0
lakindugimsara50-del <lakindugimsara50@gmail.com>
2026-09-24 14:05:54 +0530

    Sync movies_data.js for DC

--- website/data/movies_data.js
+++ website/data/movies_data.js
@@ -18,7 +18,120 @@ window.FILMSUB_DATA = {
       "slug": "oppenheimer",
       "title": "Oppenheimer",
       "subtitles": []
+    },
+    {
+      "id": "dc-2026",
+      "slug": "dc-2026",
+      "title": "DC",
+      "title_si": "",
+      "year": 2026,
+      "imdb": "7.9",
+      "rating": "7.9",
+      "imdb_id": "tt37501035",
+      "tmdb_id": "1479832",
+      "type": "movie",
+      "season": null,
+      "episode": null,
+      "episode_title": "",
+      "number_of_seasons": 0,
+      "number_of_episodes": 0,
+      "seasons": [],
+      "poster": "https://image.tmdb.org/t/p/w500/8k71CD24GIAmEFgHYRhqFvwbXH8.jpg",
+      "poster_url": "https://image.tmdb.org/t/p/w500/8k71CD24GIAmEFgHYRhqFvwbXH8.jpg",
+      "backdrop": "https://image.tmdb.org/t/p/original/9GmAODqkVdRvSgg5C4Vfeomi5Nj.jpg",
+      "backdrop_url": "https://image.tmdb.org/t/p/original/9GmAODqkVdRvSgg5C4Vfeomi5Nj.jpg",
+      "genres": [
+        "Romance",
+        "Action",
+        "Drama"
+      ],
+      "language": "English",
+      "subtitle_language": "Sinhala",
+      "quality": "720p",
+      "duration": "142 min",
+      "description": "Rebel Devadas  joins forces with Chandra in a dangerous world where passion and peril collide. Their turbulent alliance demands bloodshed and tests loyalties as survival becomes the ultimate stake in their fight.",
+      "director": "Arun Matheswaran",
+      "cast": [
+        {
+          "name": "Lokesh Kanagaraj",
+          "character": "Devadas"
+        },
+        {
+          "name": "Wamiqa Gabbi",
+          "character": "Chandra"
+        },
+        {
+          "name": "Sanjana Krishnamoorthy",
+          "character": "Parvathi"
+        },
+        {
+          "name": "Avinash Raghudevan",
+          "character": "Kitty"
+        },
+        {
+          "name": "Jawahar Sakthi",
+          "character": "Karuppan"
+        }
+      ],
+      "featured": false,
+      "trending": true,
+      "file_id": "",
+      "message_id": 0,
+      "file_name": "sub_web_dc-2026.mp4",
+      "file_size": 1545230105,
+      "stream_url": "https://drive.google.com/file/d/1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g/preview",
+      "streams": [
+        {
+          "server": "Server 1",
+          "label": "⚡ Server 1 (Google Drive Ultra HD)",
+          "type": "video/mp4",
+          "stream_url": "https://drive.google.com/file/d/1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g/preview"
+        }
+      ],
+      "downloads": [
+        {
+          "quality": "1080p (Cloud High-Speed)",
+          "size": "1.4 GB",
+          "url": "https://drive.google.com/uc?export=download&id=1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g",
+          "format": "MP4",
+          "host": "Google Drive"
+        },
+        {
+          "quality": "720p",
+          "size": "810.5 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g",
+          "format": "MP4",
+          "host": "Direct"
+        },
+        {
+          "quality": "480p",
+          "size": "412.6 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g",
+          "format": "MP4",
+          "host": "Direct"
+        },
+        {
+          "quality": "360p",
+          "size": "265.3 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1twmgGk7CRqLWhUQtBhRD7z7V8qiMNC9g",
+          "format": "MP4",
+          "host": "Direct"
+        }
+      ],
+      "subtitles": [
+        {
+          "language": "Sinhala",
+          "label": "Sinhala Subtitle",
+          "url": "data:text/vtt;charset=utf-8,WEBVTT%0A%0A1%0A00%3A00%3A01.000%20--%3E%2000%3A00%3A06.000%0AFilmSub.lk%20%E0%B7%80%E0%B7%99%E0%B6%AD%E0%B7%92%E0%B6%B1%E0%B7%8A%20%E0%B7%83%E0%B7%92%E0%B6%82%E0%B7%84%E0%B6%BD%20%E0%B6%8B%E0%B6%B4%E0%B7%83%E0%B7%92%E0%B6%BB%E0%B7%90%E0%B7%83%E0%B7%92%20%E0%B7%83%E0%B6%B8%E0%B6%9F%0A%0A2%0A00%3A00%3A07.000%20--%3E%2000%3A00%3A15.000%0ADC%20%282026%29%20%E0%B6%B1%E0%B7%90%E0%B6%BB%E0%B6%B9%E0%B7%93%E0%B6%B8%E0%B6%A7%20%E0%B7%83%E0%B7%8A%E0%B6%AD%E0%B7%96%E0%B6%AD%E0%B7%92%E0%B6%BA%E0%B7%92%21",
+          "default": true
+        }
+      ],
+      "source_method": "torrent",
+      "site_url": "https://filmsub.pages.dev/movie.html?id=dc-2026",
+      "added_date": "2026-09-24",
+      "added_at": "2026-09-24T08:35:53Z",
+      "added_by": "bot_auto_leech"
     }
   ],
-  "last_updated": "2026-09-24T08:32:31Z"
+  "last_updated": "2026-09-24T08:35:53Z"
 };

ff378e1b2f14dcd9480657a9c7565648c3376ed1
lakindugimsara50-del <lakindugimsara50@gmail.com>
2026-09-24 14:16:04 +0530

    Sync movies_data.js for One Last Shot

--- website/data/movies_data.js
+++ website/data/movies_data.js
@@ -18,7 +18,119 @@ window.FILMSUB_DATA = {
       "slug": "oppenheimer",
       "title": "Oppenheimer",
       "subtitles": []
+    },
+    {
+      "id": "one-last-shot-2026",
+      "slug": "one-last-shot-2026",
+      "title": "One Last Shot",
+      "title_si": "",
+      "year": 2026,
+      "imdb": "7.5",
+      "rating": "7.5",
+      "imdb_id": "tt37971717",
+      "tmdb_id": "1607127",
+      "type": "movie",
+      "season": null,
+      "episode": null,
+      "episode_title": "",
+      "number_of_seasons": 0,
+      "number_of_episodes": 0,
+      "seasons": [],
+      "poster": "https://image.tmdb.org/t/p/w500/niQ4NBh2jqAf1hDZP5m6ReWFAb7.jpg",
+      "poster_url": "https://image.tmdb.org/t/p/w500/niQ4NBh2jqAf1hDZP5m6ReWFAb7.jpg",
+      "backdrop": "https://image.tmdb.org/t/p/original/8giIQcHpxgsPVP6c7aQtHl3txuh.jpg",
+      "backdrop_url": "https://image.tmdb.org/t/p/original/8giIQcHpxgsPVP6c7aQtHl3txuh.jpg",
+      "genres": [
+        "Action",
+        "Thriller"
+      ],
+      "language": "English",
+      "subtitle_language": "Sinhala",
+      "quality": "1080p",
+      "duration": "98 min",
+      "description": "Navy Seal Jake Harris must stop a team of mercenaries, led by his disillusioned former comrade, from disabling the US missile defense network.",
+      "director": "James Nunn",
+      "cast": [
+        {
+          "name": "Scott Adkins",
+          "character": "Jake Harris"
+        },
+        {
+          "name": "Steven Cree",
+          "character": "Elliot Carter"
+        },
+        {
+          "name": "Hannah Ware",
+          "character": "Elena Torres"
+        },
+        {
+          "name": "Dolph Lundgren",
+          "character": "Admiral Mulholland"
+        },
+        {
+          "name": "Kane Kosugi",
+          "character": "Lee"
+        }
+      ],
+      "featured": false,
+      "trending": true,
+      "file_id": "",
+      "message_id": 0,
+      "file_name": "sub_web_one-last-shot-2026.mp4",
+      "file_size": 1549277151,
+      "stream_url": "https://drive.google.com/file/d/1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz/preview",
+      "streams": [
+        {
+          "server": "Server 1",
+          "label": "⚡ Server 1 (Google Drive Ultra HD)",
+          "type": "video/mp4",
+          "stream_url": "https://drive.google.com/file/d/1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz/preview"
+        }
+      ],
+      "downloads": [
+        {
+          "quality": "1080p (Cloud High-Speed)",
+          "size": "1.4 GB",
+          "url": "https://drive.google.com/uc?export=download&id=1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz",
+          "format": "MP4",
+          "host": "Google Drive"
+        },
+        {
+          "quality": "720p",
+          "size": "812.6 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz",
+          "format": "MP4",
+          "host": "Direct"
+        },
+        {
+          "quality": "480p",
+          "size": "413.7 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz",
+          "format": "MP4",
+          "host": "Direct"
+        },
+        {
+          "quality": "360p",
+          "size": "266.0 MB",
+          "url": "https://drive.google.com/uc?export=download&id=1hYF0YAOSo6l0TRLxPxH_kViOn79DQGQz",
+          "format": "MP4",
+          "host": "Direct"
+        }
+      ],
+      "subtitles": [
+        {
+          "language": "Sinhala",
+          "label": "Sinhala Subtitle",
+          "url": "data:text/vtt;charset=utf-8,WEBVTT%0A%0A1%0A00%3A00%3A01.000%20--%3E%2000%3A00%3A06.000%0AFilmSub.lk%20%E0%B7%80%E0%B7%99%E0%B6%AD%E0%B7%92%E0%B6%B1%E0%B7%8A%20%E0%B7%83%E0%B7%92%E0%B6%82%E0%B7%84%E0%B6%BD%20%E0%B6%8B%E0%B6%B4%E0%B7%83%E0%B7%92%E0%B6%BB%E0%B7%90%E0%B7%83%E0%B7%92%20%E0%B7%83%E0%B6%B8%E0%B6%9F%0A%0A2%0A00%3A00%3A07.000%20--%3E%2000%3A00%3A15.000%0AOne%20Last%20Shot%20%282026%29%20%E0%B6%B1%E0%B7%90%E0%B6%BB%E0%B6%B9%E0%B7%93%E0%B6%B8%E0%B6%A7%20%E0%B7%83%E0%B7%8A%E0%B6%AD%E0%B7%96%E0%B6%AD%E0%B7%92%E0%B6%BA%E0%B7%92%21",
+          "default": true
+        }
+      ],
+      "source_method": "torrent",
+      "site_url": "https://filmsub.pages.dev/movie.html?id=one-last-shot-2026",
+      "added_date": "2026-09-24",
+      "added_at": "2026-09-24T08:46:03Z",
+      "added_by": "bot_auto_leech"
     }
   ],
-  "last_updated": "2026-09-24T08:44:06Z"
+  "last_updated": "2026-09-24T08:46:03Z"
 };
